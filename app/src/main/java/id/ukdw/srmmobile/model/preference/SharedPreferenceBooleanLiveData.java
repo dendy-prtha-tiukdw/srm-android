d0fb1e4bfd283c15265023ef4f26160135280dfe
yose kornelius <yose.kornelius@ti.ukdw.ac.id>
2020-08-12 07:21:14 +0700

    tambah codebased SharedpreferenceLivedata dan tambah Codebased SilentSignIn

---
+++ app/src/main/java/id/ukdw/srmmobile/model/preference/SharedPreferenceBooleanLiveData.java
@@ -0,0 +1,16 @@
+package id.ukdw.srmmobile.model.preference;
+
+import android.content.SharedPreferences;
+
+public class SharedPreferenceBooleanLiveData extends SharedPreferenceLiveData<Boolean>{
+
+    public SharedPreferenceBooleanLiveData(SharedPreferences prefs, String key, Boolean defValue) {
+        super(prefs, key, defValue);
+    }
+
+    @Override
+    Boolean getValueFromPreferences(String key, Boolean defValue) {
+        return sharedPrefs.getBoolean(key, defValue);
+    }
+
+}

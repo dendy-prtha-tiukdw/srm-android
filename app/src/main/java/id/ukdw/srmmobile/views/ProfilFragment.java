88b07ecb66186ae2c533313354547ef6c9ce8fba
Varrel <54306896+Varrel-18@users.noreply.github.com>
2020-08-14 14:12:42 +0700

    UI User Profile (Glide Untuk Gambar Profile)

--- app/src/main/java/id/ukdw/srmmobile/views/ProfilFragment.java
+++ app/src/main/java/id/ukdw/srmmobile/views/ProfilFragment.java
@@ -9,13 +9,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import id.ukdw.srmmobile.R;
 
 public class ProfilFragment extends Fragment {
 
+    CircleImageView imageView;
+    String link;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        View v = inflater.inflate(R.layout.fragment_profil, container, false);
+        return v;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageView = (CircleImageView) view.findViewById(R.id.fotoProfil);
+        Glide.with(this)
+                .load(link)
+                .circleCrop()
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(imageView);
+    }
+
 }

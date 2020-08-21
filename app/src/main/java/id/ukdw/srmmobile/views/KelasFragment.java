d000f7129dcd91448398178934f06d5b916a766b
Varrel <54306896+Varrel-18@users.noreply.github.com>
2020-08-21 16:44:54 +0700

    Daftar Kelas Tambahan desain pada Recyclerview KelasFragment Onclick pada tiap recyclerview ke detailKelas_activity

--- app/src/main/java/id/ukdw/srmmobile/views/KelasFragment.java
+++ app/src/main/java/id/ukdw/srmmobile/views/KelasFragment.java
@@ -1,5 +1,6 @@
 package id.ukdw.srmmobile.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,44 +13,53 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import id.ukdw.srmmobile.R;
 
-public class KelasFragment extends Fragment {
+public class KelasFragment extends Fragment  {
     View v;
 
     public KelasFragment() {
     }
 
     RecyclerView recyclerView;
-    List<RecyclerViewModel> itemList;
+    ArrayList <RecyclerViewModelKelas> itemList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.fragment_kelas, container, false);
-
         recyclerView = v.findViewById(R.id.recyclerKelas);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        KelasAdapter kelasAdapter = new KelasAdapter(getContext(),initData());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(kelasAdapter);
 
-        recyclerView.setAdapter(new PengumumanAdapter(initData()));
-
+        kelasAdapter.setOnItemClickListener( new KelasAdapter.OnItemListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent;
+                intent = new Intent( getActivity(), detailKelas_activity.class );
+                System.out.println("TESTSTSDADA"+itemList.get( position ));
+                intent.putExtra( "RecyclerViewModelKelas", itemList.get( position ) );
+                startActivity( intent );
+            }
+        } );
         return v;
     }
 
-    private List<RecyclerViewModel> initData(){
+    private ArrayList<RecyclerViewModelKelas> initData(){
         itemList=new ArrayList<>();
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
-        itemList.add(new RecyclerViewModel(R.drawable.ic_news_admin, "Daftar Kelas"));
+        itemList.add(new RecyclerViewModelKelas("Administrasi Sistem (A)", "Detail Matakuliah"));
+        itemList.add(new RecyclerViewModelKelas("Teknik Komputer (B)", "Detail Matakuliah"));
+        itemList.add(new RecyclerViewModelKelas("Jaringan Nirkabel (C)", "Detail Matakuliah"));
+        itemList.add(new RecyclerViewModelKelas("Pendidikan Pancasila (B)", "Detail Matakuliah"));
+        itemList.add(new RecyclerViewModelKelas("Pemrograman Web (D)", "Detail Matakuliah"));
+        itemList.add(new RecyclerViewModelKelas("Sistem Basis Data (A)", "Detail Matakuliah"));
 
         return itemList;
     }
+
+
 
 }

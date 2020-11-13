1d532b8da7c09688ae8b7cb234ac48aadd5aa28f
yose kornelius <yose.kornelius@ti.ukdw.ac.id>
2020-11-13 08:25:50 +0700

    Refactor Fragment Calender - Add Calender View - Refactor List event, just one day

--- app/src/main/java/id/ukdw/srmmobile/ui/calendar/KalenderFragment.java
+++ app/src/main/java/id/ukdw/srmmobile/ui/calendar/KalenderFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +14,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.model.Event;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import id.ukdw.srmmobile.BR;
 import id.ukdw.srmmobile.R;
@@ -25,16 +33,20 @@ import id.ukdw.srmmobile.ui.daftarkelas.DaftarKelasAdapter;
 import id.ukdw.srmmobile.ui.daftarkelas.DaftarKelasFragment;
 import id.ukdw.srmmobile.ui.home.HomeActivity;
 import id.ukdw.srmmobile.ui.home.HomeViewModel;
+import lombok.SneakyThrows;
 
 public class KalenderFragment extends BaseFragment<FragmentKalenderBinding, KalenderViewModel>
-    implements KalenderNavigator{
+        implements KalenderNavigator {
 
     List<RecyclerViewModelKalender> itemlist;
+    java.util.Calendar jCalendar = java.util.Calendar.getInstance();
+    FragmentKalenderBinding fragmentKalenderBinding;
+    CalendarView calendarView;
 
     public static KalenderFragment newInstance() {
         Bundle args = new Bundle();
         KalenderFragment fragment = new KalenderFragment();
-        fragment.setArguments(args);
+        fragment.setArguments( args );
         return fragment;
     }
 
@@ -48,17 +60,54 @@ public class KalenderFragment extends BaseFragment<FragmentKalenderBinding, Kale
         return R.layout.fragment_kalender;
     }
 
+    @SneakyThrows
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated( view, savedInstanceState );
-        mViewModel.testGoogleCalendar();
+        //mViewModel.testGoogleCalendar();
+
+        Date c = Calendar.getInstance().getTime();
+        jCalendar.setTime( c );
+        jCalendar.set( jCalendar.HOUR_OF_DAY, 23 );
+        jCalendar.set( jCalendar.MINUTE, 59 );
+        jCalendar.set( jCalendar.SECOND, 59 );
+        jCalendar.setTimeZone( TimeZone.getDefault() );
+        DateTime min = new DateTime( c, TimeZone.getDefault() );
+        DateTime max = new DateTime( jCalendar.getTime() );
+        mViewModel.getListEvent( min, max );
+
+
+        getViewDataBinding().calendarView.setOnDateChangeListener( new CalendarView.OnDateChangeListener() {
+            @SneakyThrows
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                //karna month mulai dari 0
+                month = month + 1;
+
+                String dateChanged = year + "-" + month + "-" + dayOfMonth;
+
+                DateFormat df = new SimpleDateFormat( "yyyy-MM-dd" );
+                jCalendar.setTime( df.parse( dateChanged ) );
+
+                DateTime min = new DateTime( jCalendar.getTime() );
+                jCalendar.set( jCalendar.HOUR_OF_DAY, 23 );
+                jCalendar.set( jCalendar.MINUTE, 59 );
+                jCalendar.set( jCalendar.SECOND, 59 );
+                jCalendar.setTimeZone( TimeZone.getDefault() );
+                DateTime max = new DateTime( jCalendar.getTime() );
+                mViewModel.getListEvent( min, max );
+
+            }
+        } );
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
+        fragmentKalenderBinding = getViewDataBinding();
         mViewModel.setNavigator( this );
         mViewModel.setContext( getBaseActivity() );
+
     }
 
     @Override
@@ -73,31 +122,38 @@ public class KalenderFragment extends BaseFragment<FragmentKalenderBinding, Kale
 
         // Set title bar
         ((HomeActivity) getActivity())
-                .setActionBarTitle("Kalender");
+                .setActionBarTitle( "Kalender" );
     }
 
     @Override
     public void onGetListEventCalender(List<Event> kalenderEvent) {
-        itemlist = new ArrayList<>(  );
+        if (kalenderEvent.isEmpty()) {
+            getViewDataBinding().recyclerKalender.setVisibility( View.GONE );
+
+        }
+        itemlist = new ArrayList<>();
         for (Event event : kalenderEvent) {
             DateTime start = event.getStart().getDateTime();
-            String Start = ""+ start;
+            String Start = "" + start;
             if (start == null) {
                 // All-day events don't have start times, so just use
                 // the start date.
                 start = event.getStart().getDate();
 
             }
-            getViewDataBinding().recyclerKalender.setHasFixedSize(true);
-            KalenderAdapter kalenderAdapter = new KalenderAdapter(getContext(), itemlist);
-            getViewDataBinding().recyclerKalender.setLayoutManager(new LinearLayoutManager(getActivity()));
-            getViewDataBinding().recyclerKalender.setAdapter(kalenderAdapter);
+            getViewDataBinding().recyclerKalender.setHasFixedSize( true );
+            getViewDataBinding().recyclerKalender.setVisibility( View.VISIBLE );
+            KalenderAdapter kalenderAdapter = new KalenderAdapter( getContext(), itemlist );
+            getViewDataBinding().recyclerKalender.setLayoutManager( new LinearLayoutManager( getActivity() ) );
+            getViewDataBinding().recyclerKalender.setAdapter( kalenderAdapter );
 
             //Log.i(TAG, "testGoogleCalendar: " + String.format("%s (%s)", event.getSummary(), start));
-            itemlist.add( new RecyclerViewModelKalender(
-                    event.getSummary(),
-                    Start
-            ) );
+
+                itemlist.add( new RecyclerViewModelKalender(
+                        event.getSummary(),
+                        Start
+                ) );
+
         }
         getBaseActivity().hideLoading();
 
